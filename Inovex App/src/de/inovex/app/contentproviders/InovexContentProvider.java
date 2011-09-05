9abe868306e0803a2c1196b6775513a448ab6377
Renard Wellnitz <Renard.Wellnitz@googlemail.com>
2011-09-05 14:53:54 +0200

    inital commit

---
+++ Inovex App/src/de/inovex/app/contentproviders/InovexContentProvider.java
@@ -0,0 +1,251 @@
+package de.inovex.app.contentproviders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class InovexContentProvider extends ContentProvider {
+
+	private final static String TAG = InovexContentProvider.class.toString();
+	private static final String AUTHORITY = "de.inovex.app";
+
+	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+	public static class Types {
+		public static final String JOURNEY_START = "0";
+		public static final String JOURNEY_END = "1";
+		public static final String JOURNEY_CONTINUATION = "2";
+		public static final String RECEIPT = "10";
+
+	}
+
+	public static class Columns {
+
+		public static final String ID = "_id";
+		public static final String PARENT_ID = "parent_id";
+		public static final String CREATED = "created";
+		public static final String DESCRIPTION = "description";
+		public static final String START_LOCATION = "start_location";
+		public static final String DESTINATION = "destination";
+		public static final String IMAGE_PATH_URI = "image_uri";
+		public static final String TYPE = "type";
+	}
+
+	private static final UriMatcher sUriMatcher;
+	private static final int JOURNEY = 0;
+	private static final int JOURNEYS = 1;
+	private static final int RECEIPT = 2;
+	private static final int RECEIPTS = 3;
+
+	static {
+		sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		sUriMatcher.addURI(AUTHORITY, "journeys/#", JOURNEY);
+		sUriMatcher.addURI(AUTHORITY, "journeys", JOURNEYS);
+		sUriMatcher.addURI(AUTHORITY, "receipts/#", RECEIPT);
+		sUriMatcher.addURI(AUTHORITY, "receipts", RECEIPTS);
+	}
+
+	private static class DBHelper extends SQLiteOpenHelper {
+
+		private static final String TABLE_NAME = "data";
+		private static final int DATABASE_VERSION = 0;
+
+		private static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" + Columns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + Columns.PARENT_ID + " INTEGER DEFAULT -1, "
+				+ Columns.CREATED + " INTEGER, " + Columns.IMAGE_PATH_URI + " TEXT, " + Columns.START_LOCATION + " TEXT, " + Columns.DESTINATION + " TEXT, " + Columns.DESCRIPTION + " TEXT, "
+				+ Columns.TYPE + " TEXT );";
+
+		private static final String DATABASE_NAME = "inovex_app";
+
+		DBHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(TABLE_CREATE);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.w(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+			onCreate(db);
+		}
+
+	}
+
+	private DBHelper dbHelper;
+
+	@Override
+	public boolean onCreate() {
+		dbHelper = new DBHelper(getContext());
+		return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		qb.setTables(DBHelper.TABLE_NAME);
+		String limit = null;
+		List<String> args = Arrays.asList(selectionArgs);
+		int type = sUriMatcher.match(uri);
+
+		switch (type) {
+		case JOURNEY:
+		case JOURNEYS:
+		case RECEIPT:
+		case RECEIPTS:
+			selection = modifySelectionForType(uri, selection, args, type);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+
+		// If no sort order is specified use the default
+		String orderBy;
+		if (TextUtils.isEmpty(sortOrder)) {
+			orderBy = "created DESC";
+		} else {
+			orderBy = sortOrder;
+		}
+
+		// Get the database and run the query
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		Cursor c = qb.query(db, projection, selection, args.toArray(new String[] {}), Columns.ID, null, orderBy, limit);
+
+		// Tell the cursor what uri to watch, so it knows when its source data
+		// changes
+		c.setNotificationUri(getContext().getContentResolver(), uri);
+		return c;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		switch (sUriMatcher.match(uri)) {
+		case JOURNEY:
+			return "vnd.android.cursor.dir/vnd.inovex.journey";
+		case JOURNEYS:
+			return "vnd.android.cursor.item/vnd.ocr.journeys";
+		case RECEIPT:
+			return "vnd.android.cursor.dir/vnd.inovex.receipt";
+		case RECEIPTS:
+			return "vnd.android.cursor.item/vnd.ocr.receipts";
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+
+		if (values != null) {
+			values = new ContentValues(values);
+		} else {
+			values = new ContentValues();
+		}
+
+		Long now = Long.valueOf(System.currentTimeMillis());
+		values.put(Columns.CREATED, now);
+
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		long rowId = db.insert(DBHelper.TABLE_NAME, null, values);
+		if (rowId > 0) {
+			Uri entryUri = ContentUris.withAppendedId(CONTENT_URI, rowId);
+			getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+			return entryUri;
+		} else {
+			throw new SQLException("Failed to insert row into " + uri);
+		}
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		List<String> args = Arrays.asList(selectionArgs);
+		int count = 0;
+		int type = sUriMatcher.match(uri);
+
+		switch (type) {
+		case RECEIPTS:
+		case JOURNEYS:
+		case RECEIPT:
+		case JOURNEY:
+			selection = modifySelectionForType(uri, selection, args, type);
+
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+
+		count = db.delete(DBHelper.TABLE_NAME, selection, args.toArray(new String[] {}));
+
+		getContext().getContentResolver().notifyChange(uri, null);
+		getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+		return count;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		List<String> args = Arrays.asList(selectionArgs);
+		int count = 0;
+		int type = sUriMatcher.match(uri);
+		switch (type) {
+		case RECEIPTS:
+		case JOURNEYS:
+		case RECEIPT:
+		case JOURNEY:
+			selection = modifySelectionForType(uri, selection, args, type);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+		count = db.update(DBHelper.TABLE_NAME, values, selection, args.toArray(new String[] {}));
+		getContext().getContentResolver().notifyChange(uri, null);
+		return count;
+	}
+
+	private String modifySelectionForType(Uri uri, String orgSelection, List<String> list, int type) {
+		String selection = "";
+		switch (type) {
+		case RECEIPTS:
+			if (orgSelection.length() == 0) {
+				selection = Columns.TYPE + " >=?";
+			} else {
+				selection = orgSelection + " AND " + Columns.TYPE + ">=?";
+			}
+			list.add(Types.RECEIPT);
+			break;
+		case JOURNEYS:
+			if (orgSelection.length() == 0) {
+				selection = Columns.TYPE + " <?";
+			} else {
+				selection = orgSelection + " AND " + Columns.TYPE + "<?";
+			}
+			list.add(Types.RECEIPT);
+			break;
+		case RECEIPT:
+		case JOURNEY:
+			String id = uri.getLastPathSegment();
+			selection = Columns.ID + "=?";
+			list.add(id);
+			break;
+		}
+		return selection;
+	}
+
+}
