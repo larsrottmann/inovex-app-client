c76b4fe710e730f376c524da026998b619d0d747
Renard Wellnitz <Renard.Wellnitz@googlemail.com>
2011-09-06 13:30:29 +0200

    added compound view for datetime picking

---
+++ Inovex App/src/de/inovex/app/views/DateTimePicker.java
@@ -0,0 +1,24 @@
+package de.inovex.app.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import de.inovex.app.R;
+
+public class DateTimePicker extends RelativeLayout {
+
+	Button mDateButton;
+	Button mTimeButton;
+	
+	public DateTimePicker(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.date_time_picker, this);
+		mDateButton = (Button) findViewById(R.id.buttonDate);
+		mTimeButton = (Button) findViewById(R.id.buttonTime);
+		
+	}
+
+}
