7842bcbf1f7163784d44dac78e9787f3568748af
Jonas Gehring <jonas.gehring@inovex.de>
2011-09-15 12:04:24 +0200

    unit test for search

--- Inovex App Test/src/de/inovex/activites/contacts/ListContactsActivityTest.java
+++ Inovex App Test/src/de/inovex/activites/contacts/ListContactsActivityTest.java
@@ -2,24 +2,38 @@ package de.inovex.activites.contacts;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.jayway.android.robotium.solo.Solo;
+
 import de.inovex.app.R;
 import de.inovex.app.activities.contacts.ListContactsActivity;
 
 public class ListContactsActivityTest extends ActivityInstrumentationTestCase2<ListContactsActivity> {
 	private ListContactsActivity mActivity;
+	private Solo solo;
 
 	public ListContactsActivityTest() {
-		super("de.inovex.app.activites.contacts", ListContactsActivity.class);
+		super("de.inovex.app", ListContactsActivity.class);
 	}
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		mActivity = getActivity();
+		solo = new Solo(getInstrumentation(), mActivity);
+	}
 
+	@Override
+	protected void tearDown() throws Exception {
+		try {
+			// run finalize on solo will bring: inject event permission exception
+			//solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		super.tearDown();
 	}
 
 	public void testAllContacts() {
@@ -38,20 +52,34 @@ public class ListContactsActivityTest extends ActivityInstrumentationTestCase2<L
 		assertEquals(View.GONE, view.getVisibility());
 	}
 
-	public void testSearch() throws InterruptedException {
-		// suche button
+	public void testSearch() throws Throwable {
+		runTestOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				mActivity.onSearchRequested();
+			}
+		});
+		getInstrumentation().waitForIdleSync();
+		solo.sleep(2000);
+		solo.enterText(0, "geh");
+		solo.sendKey(Solo.ENTER);
+		solo.sendKey(Solo.ENTER);
+		getInstrumentation().waitForIdleSync();
+		solo.sleep(2000);
+		assertEquals("Search results for 'geh':", solo.getText(1).getText());
 
-		// text eingeben
-		synchronized (this) {
-			Thread.sleep(5000);
-		}
-		EditText ed = (EditText) mActivity.getCurrentFocus();
-		ed.setText("geh");
-		synchronized (this) {
-			Thread.sleep(5000);
-		}
-		// list item prüfen
-		// search label
-		// search button
+		ListView contacts = (ListView) mActivity.findViewById(R.id.list_contacts);
+		assertEquals(2, contacts.getCount());
+
+		// item
+		TextView tv = (TextView) contacts.getChildAt(0).findViewById(R.id.contacts_item_title);
+		assertEquals("Jan Gehring", tv.getText());
+		tv = (TextView) contacts.getChildAt(0).findViewById(R.id.contacts_item_details);
+		assertEquals("Operations, Pforzheim", tv.getText());
+
+		View view = mActivity.findViewById(R.id.contacts_search_label);
+		assertEquals(View.VISIBLE, view.getVisibility());
+		view = mActivity.findViewById(R.id.contacts_show_all);
+		assertEquals(View.VISIBLE, view.getVisibility());
 	}
 }
